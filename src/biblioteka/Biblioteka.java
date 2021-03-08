b2cc40690cc13d92667f7332228af054834a4088
milicaklaric <milklaric15@gmail.com>
2021-03-08 13:00:20 +0100

    Dodata je logicka kontrola u klasu Biblioteka na drugi nacin

--- src/biblioteka/Biblioteka.java
+++ src/biblioteka/Biblioteka.java
@@ -9,6 +9,11 @@ public class Biblioteka implements BibliotekaInterface {
 	private LinkedList<Knjiga> knjige = new LinkedList<Knjiga>();
 	@Override
 	public void dodajKnjigu(Knjiga knjiga) {
+		if (knjiga == null)
+			throw new NullPointerException("Knjiga ne sme biti null");
+		
+		if (knjige.contains(knjiga))
+			throw new RuntimeException("Knjiga vec postoji u biblioteci");
 		knjige.add(knjiga);
 
 	}
@@ -26,6 +31,9 @@ public class Biblioteka implements BibliotekaInterface {
 
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac) {
+		if (autor == null && isbn == null && naslov == null && izdavac == null)
+			return knjige;
+		
 		LinkedList<Knjiga> rezultat = new LinkedList<Knjiga>();
 		for (Knjiga knjiga : knjige) {
 			if (knjiga.getNaslov().contains(naslov)) {
