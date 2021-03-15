d635276d15cd14a5c09a557da0456f075f59bbc4
milicaklaric <milklaric15@gmail.com>
2021-03-15 16:04:29 +0100

    Dodata dokumentacija za interfejs BibliotekaInterfejs

--- src/biblioteka/interfejs/BibliotekaInterface.java
+++ src/biblioteka/interfejs/BibliotekaInterface.java
@@ -5,9 +5,23 @@ import java.util.LinkedList;
 import biblioteka.Autor;
 import biblioteka.Knjiga;
 
+/**
+ * Interfejsk koji predstavlja biblioteku
+ * @author Milica Klaric
+ *
+ */
 public interface BibliotekaInterface {
 
+	/**
+	 * Dodaje novu knjigu u biblioteku.
+	 * 
+	 * @param knjiga Knjiga koja se dodaje
+	 * 
+	 * @throws java.lang.NullPointerException ako je dodata knjiga null
+	 * @throws java.lang.RuntimeException ako je knjiga vec postoji u biblioteci
+	 */
 	public void dodajKnjigu(Knjiga knjiga);
+	
 	
 	public void obrisiKnjigu(Knjiga knjiga);
 	
